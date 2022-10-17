a0622835ee59f27e141c565e8e36b25a2345d54a
Daria Dmitricheva <110775372+12data12@users.noreply.github.com>
2022-10-17 14:46:23 +0300

    Task1 correction

--- Task1.py
+++ Task1.py
@@ -1,6 +1,6 @@
 # Задайте список из нескольких чисел. Напишите программу, которая найдёт сумму элементов списка, стоящих на нечётной позиции.
 
 import random
-n = [round(random.uniform (-99.99, 100), 2) for x in range (10)]
-print(f'List of random real numbers: {n}')
-print (f'The sum of the list elements having odd indexes is {round((sum(n[::2]) if n else 0),2)}')
+numbers = [round(random.uniform (-99.99, 100), 2) for x in range (10)]
+print(f'List of random real numbers: {numbers}')
+print (f'The sum of the list elements having odd indexes is {round((sum(numbers[::2]) if numbers else 0),2)}')
